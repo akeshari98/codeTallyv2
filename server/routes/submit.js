569b0c35e6d2d3a6698cddc1d1ca81af15c1eda6
akeshari986 <22bcs055@iiitdmj.ac.in>
2024-08-10 15:49:20 +0530

    first commit

---
+++ server/routes/submit.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {submissionQueue,waitforJobCompletion} = require('../jobs/submissionWorker')
+
+router.post('/', async (req, res) => {
+  const { code, problem_id } = req.body;
+  if (!code || !problem_id) {
+    return res.status(400).send('Code and problem ID are required.');
+  }
+
+  const job = await submissionQueue.add('submissionProcess',{code: code, problemId: problem_id});
+  const verdictData = await waitforJobCompletion(submissionQueue,job);
+  res.send(verdictData);
+
+});
+
+module.exports = router;
